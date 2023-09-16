46e2f961a053760a693cbb12238351ed302bb726
Fukaya Temma <23367170+pranc1ngpegasus@users.noreply.github.com>
2023-09-16 18:28:39 +0900

    fix: return basiliskk error on error (#6)

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ mod domain;
 mod infra;
 
 use adapter::listener::ListenerImpl;
+use domain::error::BasiliskkErr;
 use domain::listener::Listener;
 use infra::logger;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), BasiliskkErr> {
     logger::init();
 
-    ListenerImpl::new(1178).listen().await.unwrap()
+    ListenerImpl::new(1178).listen().await
 }
